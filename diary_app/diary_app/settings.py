c64d0db00db0380c04763851f105917bdf02785b
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-09-19 21:52:57 -0600

    feat(initial setup): adding .pg_service config for local dev

--- diary_app/diary_app/settings.py
+++ diary_app/diary_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,8 +75,10 @@ WSGI_APPLICATION = 'diary_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "OPTIONS": {
+          "service": "diary_app_db_service",
+        }
     }
 }
 
