e12c7a826bab0da7b789845728a690580324ff48
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-10-03 21:43:25 -0600

    feat(models): modify diary_entries and emotions models

--- diary_app/diary_entries/models.py
+++ diary_app/diary_entries/models.py
@@ -3,24 +3,27 @@ from django.db import models
 
 # Create your models here.
 
-class Diary_Entry(models.Model): 
-  entry_field = models.CharField(max_length=2500)
-  published_date = models.DateTimeField("date published")
-  emotion_descriptors = models.ForeignKey(Emotions) 
 
 class Emotions(models.Model):
   
-  EMOTIONS = {
-    "happiness",
-    "sadness",
-    "anger",
-    "fear",
-    "surprise",
-    "disgust"
-  }
+  EMOTIONS_CUSTOM_CODE = [
+    ("HP", "happines"),
+    ("SN", "sadness"),
+    ("AG", "anger"),
+    ("FR", "fear"),
+    ("SP", "surprise"),
+    ("DG", "disgust")
+  ]
   
-  emotion_name = models.CharField(max_length=25, choices=EMOTIONS)
+  emotions_custom_code = models.CharField(max_length=2, choices=EMOTIONS_CUSTOM_CODE)
+  emotion_name = models.CharField(max_length=25)
   emotion_description = models.CharField(max_length=100)
-  emotion_value = models.DecimalField(max_digits=1, decimal_places=0)
+  
+
+class Diary_Entry(models.Model): 
+  entry_field = models.CharField(max_length=2500)
+  published_date = models.DateTimeField("date published")
+  emotion_descriptors = models.ManyToManyField(Emotions, db_table="diary_entry_emotions") 
+
 
   
