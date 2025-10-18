51f8b3e4f79d2958a8386c175d3eaebff4bc3874
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-10-17 20:09:45 -0600

    feat(models): modify diary_entries and emotions models

--- diary_app/diary_entries/models.py
+++ diary_app/diary_entries/models.py
@@ -1,5 +1,8 @@
+import datetime
+
 from django.db import models
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -18,12 +21,22 @@ class Emotions(models.Model):
   emotions_custom_code = models.CharField(max_length=2, choices=EMOTIONS_CUSTOM_CODE)
   emotion_name = models.CharField(max_length=25)
   emotion_description = models.CharField(max_length=100)
+
+  
+  def __str__(self):
+    return self.emotion_name
   
 
 class Diary_Entry(models.Model): 
   entry_field = models.CharField(max_length=2500)
   published_date = models.DateTimeField("date published")
-  emotion_descriptors = models.ManyToManyField(Emotions, db_table="diary_entry_emotions") 
+  emotion_descriptors = models.ManyToManyField(Emotions, db_table="diary_entry_emotions")
+
+  def __str__(self):
+    return self.entry_field
+
+  def was_published_recently(self):
+    return self.published_date >= timezone.now() - datetime.timedelta(days=1) 
 
 
   
